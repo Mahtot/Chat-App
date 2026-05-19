19cbea4a087fda6db5232617f2228c9844ff2247
mahtot <mahtotgher13@gmail.com>
2026-05-19 18:47:24 +0300

    feat: request permission to send notifications and get an address of the device using firebase messaging

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    final notificationSettings = await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print('token: $token');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +47,13 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(children: [
-        const SizedBox(height: 12),
-        Expanded(child: ChatMessages()), NewMessage()]),
+      body: Column(
+        children: [
+          const SizedBox(height: 12),
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
     );
   }
 }
