1e6867746fdb550b817053c619f241809213cf99
mahtot <mahtotgher13@gmail.com>
2026-05-19 12:25:49 +0300

    feat: add chat messages and input widgets

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +25,7 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: const Text('Logged In')),
+      body: Column(children: [Expanded(child: ChatMessages()), NewMessage()]),
     );
   }
 }
